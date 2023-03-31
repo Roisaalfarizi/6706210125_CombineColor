2ffa145d806d905038cb8d6d30920d67f245fdf2
roisa <roisaalfarizi03092003@gmail.com>
2023-03-31 17:16:07 +0700

    membuat tema gelap dan terang

--- app/src/main/java/com/example/combinecolors/MainActivity.kt
+++ app/src/main/java/com/example/combinecolors/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         }else if(TextUtils.isDigitsOnly(warna1)){
             Toast.makeText(this, R.string.inputNumber, Toast.LENGTH_LONG).show()
             return
-        }else if (warna1 != "Merah" && warna1 != "Kuning" && warna1 != "Hijau" && warna1 != "Ungu" && warna1 != "Biru"){
+        }else if (warna1 != "Merah" && warna1 != "Kuning" && warna1 != "Hijau" && warna1 != "Ungu" && warna1 != "Biru" && warna1 != "merah" && warna1 != "kuning" && warna1 != "hijau" && warna1 != "ungu" && warna1 != "biru"){
             Toast.makeText(this, R.string.outStock, Toast.LENGTH_LONG).show()
             return
         }
@@ -35,8 +35,8 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, R.string.inputNumber, Toast.LENGTH_LONG).show()
             return
         }
-        else if ( warna2 != "Merah" && warna2 != "Kuning" && warna2 != "Hijau" && warna2 != "Ungu" && warna2 != "Biru"
-        ){
+        else if ( warna2 != "Merah" && warna2 != "Kuning" && warna2 != "Hijau" && warna2 != "Ungu" && warna2 != "Biru" &&
+            warna2 != "merah" && warna2 != "kuning" && warna2 != "hijau" && warna2 != "ungu" && warna2 != "biru"){
             Toast.makeText(this, R.string.outStock, Toast.LENGTH_LONG).show()
             return
 
@@ -77,6 +77,53 @@ class MainActivity : AppCompatActivity() {
         else if(warna1 == "Biru" && warna2 == "Ungu"|| warna1 == "Ungu" && warna2 == "Biru"){
             binding.Hasil.text = getString(R.string.Biru_tua)
         }else if(warna1 == "Biru" && warna2 == "Biru"){
+            binding.Hasil.text = getString(R.string.Biru)
+        }
+
+
+
+
+
+
+
+        //input Kecil
+
+        else if(warna1 == "merah" && warna2 == "kuning"|| warna1 == "kuning" && warna2 == "merah"){
+            binding.Hasil.text = getString(R.string.Oren)
+        }else if(warna1 == "merah" && warna2 == "hijau"|| warna1 == "hijau" && warna2 == "herah"){
+            binding.Hasil.text = getString(R.string.Coklat)
+        }else if(warna1 == "merah" && warna2 == "biru"|| warna1 == "biru" && warna2 == "merah"){
+            binding.Hasil.text = getString(R.string.Ungu)
+        }else if(warna1 == "merah" && warna2 == "ungu"|| warna1 == "ungu" && warna2 == "merah"){
+            binding.Hasil.text = getString(R.string.Pink)
+        }else if(warna1 == "merah" && warna2 == ",merah"){
+            binding.Hasil.text = getString(R.string.Merah)
+        }
+
+
+
+        else if(warna1 == "kuning" && warna2 == "hijau"|| warna1 == "hijau" && warna2 == "kuning"){
+            binding.Hasil.text = getString(R.string.Hijau_muda)
+        }else if(warna1 == "kuning" && warna2 == "biru"|| warna1 == "biru" && warna2 == "kuning"){
+            binding.Hasil.text = getString(R.string.Hijau)
+        }else if(warna1 == "kuning" && warna2 == "ungu"|| warna1 == "ungu" && warna2 == "kuning"){
+            binding.Hasil.text = getString(R.string.Krem)
+        }else if(warna1 == "kuning" && warna2 == "kuning"){
+            binding.Hasil.text = getString(R.string.Kuning)
+        }
+
+
+        else if(warna1 == "hijau" && warna2 == "biru"|| warna1 == "biru" && warna2 == "hijau"){
+            binding.Hasil.text = getString(R.string.Hijau_muda)
+        }else if(warna1 == "hijau" && warna2 == "ungu"|| warna1 == "ungu" && warna2 == "hijau"){
+            binding.Hasil.text = getString(R.string.Abu)
+        }else if(warna1 == "hijau" && warna2 == "hijau"){
+            binding.Hasil.text = getString(R.string.Hijau)
+        }
+
+        else if(warna1 == "biru" && warna2 == "ungu"|| warna1 == "ungu" && warna2 == "biru"){
+            binding.Hasil.text = getString(R.string.Biru_tua)
+        }else if(warna1 == "biru" && warna2 == "biru"){
             binding.Hasil.text = getString(R.string.Biru)
         }
     }
